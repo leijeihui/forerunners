50a0af70a997e61ef0d15fd7b9b5f6b45cb088f2
Edmund Louie <leijeihui@gmail.com>
2017-04-12 15:23:06 -0700

    add s3 controller to db

---
+++ server/db/controllers/s3Ctrl.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var S3FS = require('s3fs');
+var s3fsImpl = new S3FS('forerunnersequipmentco', {
+  accessKeyId: process.env.S3_ACCESS_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+  
+});
+
+module.exports = function(file, res) {
+  var stream = fs.createReadStream(file.path);
+  return s3fsImpl.writeFile(file.originalFilename, stream).then(function() {
+    fs.unlink(file.path, function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+    res.json(file.originalFilename);
+  });
+};

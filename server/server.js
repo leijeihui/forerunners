241315224d287185507b0853461edf0b375ba9b7
Edmund Louie <leijeihui@gmail.com>
2017-04-19 12:09:15 -0700

    add routes to server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const multiparty = require('connect-multiparty');
+const routes = require('./routes/routes.js');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -25,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/../client/public'));
 app.use(multipartyMiddleware);
+app.use('/api', routes);
 
 app.listen(port, () => {
   console.log('listening to port:', port);

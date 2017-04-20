f4f94c8072a1e834a6906348809e2890c21b909d
Edmund Louie <leijeihui@gmail.com>
2017-04-20 14:38:55 -0700

    add new react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
-import {BrowserRouter} from 'react-router-dom';
+import Landing from './components/Landing.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import {BrowserRouter as Router, Route, Link, HashRouter} from 'react-router-dom';
+import {BrowserHistory} from 'react-router';
 
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <HashRouter>
+    <div>
+      {/*<ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>*/}
+      <Route exact path="/" component={() => {
+        return <Landing />;
+      }} />
+      <Route exact path="/login" component={() => {
+        return <Login />;
+      }} />
+      <Route exact path="/signup" component={() => {
+        return <Signup />;
+      }} />
+    </div>
+  </HashRouter>
+  , 
   document.getElementById('app')
 );
